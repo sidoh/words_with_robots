d4f62021524e55f8b676086ee99645ca1f2a45de
Chris Mullins <chris.mullins10@gmail.com>
2013-04-10 01:12:15 -0700

    clean up, rename iterative deepening to fixed depth (which is what it actually is)

---
+++ src/main/java/org/sidoh/words_with_robots/move_generation/params/FixedDepthParamKey.java
@@ -0,0 +1,41 @@
+package org.sidoh.words_with_robots.move_generation.params;
+
+/**
+ * Params to be used with a fixed depth move algorithm
+ */
+public enum FixedDepthParamKey implements MoveGeneratorParamKey {
+  /**
+   * The maximum number of levels to search
+   */
+  MAXIMUM_DEPTH(4),
+
+  /**
+   * The maximum size of the move cache. The move cache is passed to sibling nodes and contains the
+   * best-scoring moves from previous sibling game states. This sometimes helps us avoid repeated
+   * work in re-generating moves for a given state.
+   */
+  MOVE_CACHE_SIZE(100),
+
+  /**
+   * The minimum score a move must have in order for us to consider it. If no moves have a score of
+   * at least this value, we'll still consider the first one.
+   */
+  MIN_SCORE(10),
+
+  /**
+   * The maximum number of moves to consider at each level. Limiting this helps reduce computation
+   * time, but comes at the cost of loss of information (meaning potentially sub-optimal moves will
+   * be generated).
+   */
+  BRANCHING_FACTOR_LIMIT(20);
+
+  private final Object defaultValue;
+  private FixedDepthParamKey(Object defaultValue) {
+    this.defaultValue = defaultValue;
+  }
+
+  @Override
+  public Object getDefaultValue() {
+    return defaultValue;
+  }
+}
--- src/main/java/org/sidoh/words_with_robots/move_generation/params/IterativeDeepeningParamKey.java
+++ src/main/java/org/sidoh/words_with_robots/move_generation/params/IterativeDeepeningParamKey.java
@@ -4,6 +4,10 @@ package org.sidoh.words_with_robots.move_generation.params;
  * Defines some special parameters for use with an iterative deepening algorithm
  */
 public enum IterativeDeepeningParamKey implements MoveGeneratorParamKey {
+  /**
+   * Maximum number of milliseconds to let the algorithm run. Iterative deepening works by
+   * iteratively incrementing the number of lookahead plies until it runs out of time.
+   */
   MAX_EXECUTION_TIME_IN_MS(10000);
 
   private final Object defaultValue;
